2220dc4d218c1755a7adf5d3eed5f8273ecc7035
Huzaifa123451 <huzaifaahmer978@gmail.com>
2025-11-09 21:59:58 -0500

    `Task 1.1`

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,41 @@
 package theater;
 
+/**
+ * Represents a play in the theater system.
+ * Each play has a name and a type.
+ *
+ * <p>This class is immutable and does not allow null fields.</p>
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
+    /**
+     * Constructs a Play object.
+     * @param name the name of the play
+     * @param type the type of the play (e.g., tragedy, comedy)
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
     }
+
+    /**
+     * Returns the name of the play.
+     * @return the play name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * Returns the type of the play.
+     *
+     * @return the play type
+     */
+
+    public String getType() {
+        return type;
+    }
 }
+
